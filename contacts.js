b11047060367401957ac1b7d4b5ce69653d3efee
Максим Литвин / Maksym Lytvyn <104524765+Maksym-Lytvyn@users.noreply.github.com>
2023-08-01 17:54:34 +0300

    up

---
+++ contacts.js
@@ -0,0 +1,61 @@
+const fs = require("fs").promises;
+const path = require("path");
+
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+const dirName = 'db';
+
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    const contacts = JSON.parse(data);
+    return contacts;
+  } catch (error) {
+    return [];
+  }
+}
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const filteredContacts = contacts.find(
+    (contactToFind) => contactToFind.id === contactId
+  );
+  console.log(filteredContacts) || console.log(null);
+}
+
+async function removeContact(contactId) {
+  try {
+    const contacts = await listContacts();
+    const index = contacts.findIndex((c) => c.id === contactId);
+    if (index !== -1) {
+      const removedContact = contacts.splice(index, 1)[0];
+      await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+      return removedContact;
+    }
+    return null;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function addContact(name, email, phone) {
+  try {
+    const contacts = await listContacts();
+    const newContact = {
+      name,
+      email,
+      phone,
+    };
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return newContact;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
---
+++ readme.txt
@@ -0,0 +1,6 @@
+Мої вітання!
+Ось посилання на скріни з моєю домашкою:
+1: Табличка з контактами - https://monosnap.com/file/dBdvi1dzfuB8ylodTCHJadgyG4qYXD
+2: Пошук контактів з айді: https://monosnap.com/file/iRGZoLUa97l2SvvRcg9mjATJpiX6Qx
+3: Додавання контактів за вказаними даними: https://monosnap.com/file/7ZYjSOgYbCQtKAGPyIWzXSMMmcQVtq
+4: Видалення контакту за айді: https://monosnap.com/file/8QV6BNWUCZwTaoLcw8GM9Qs3Mc7xZn
